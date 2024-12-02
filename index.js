02529ec7b2c8b3e036d84f7d2025a74580738b93
alecipriano <alleciprianos@gmail.com>
2024-12-02 12:17:57 -0300

    Inserido cod tabuada com for, while and dowhile

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Hi");
+
+let tab = 5;
+let num = 0;
+console.log("Tabuada com for:");
+for (let num = 0; num <= 10; num++){
+    console.log(`${tab} x ${num} = ${tab * num}`);
+}
+console.log("");
+console.log("Tabuada com While:");
+
+while(num <= 10){
+	console.log(`${tab} x ${num} = ${tab * num}`);
+	num++
+}
+
+console.log("");
+console.log("Tabuada com Do... While:");
+
+num = 0;
+do{
+	console.log(`${tab} x ${num} = ${tab * num}`);
+	num++;
+}while(num <= 10);
